72f28bf7470306a4d8b8d5850e87e3a396a4be7f
Raul Leano <theylooksotired@gmail.com>
2023-01-17 22:11:34 +0100

    Fixed problem with recipes

--- base/lib/Post/Post_Form.php
+++ base/lib/Post/Post_Form.php
@@ -11,29 +11,4 @@
 class Post_Form extends Form
 {
 
-    public function create()
-    {
-        $fields = '
-            ' . $this->field('image') . '
-            ' . $this->field('title') . '
-            ' . $this->field('short_description') . '
-            <div class="textarea_ck_editor">' . $this->field('description') . '</div>
-            ' . $this->field('categories');
-        return Form::createForm($fields, ['submit' => __('save'), 'action' => url('cuenta/subir-articulo'), 'class'=>'form_site form_site_border']);
-    }
-
-    public function edit()
-    {
-        if ($this->object->id() == '') {
-            return '';
-        }
-        $fields = '
-            ' . $this->field('image') . '
-            ' . $this->field('title') . '
-            ' . $this->field('short_description') . '
-            <div class="textarea_ck_editor">' . $this->field('description') . '</div>
-            ' . $this->field('categories');
-        return Form::createForm($fields, ['submit' => __('save'), 'action' => url('cuenta/editar-articulo/' . $this->object->id()), 'class'=>'form_site form_site_border']);
-    }
-
 }
--- base/lib/Recipe/Recipe_Form.php
+++ base/lib/Recipe/Recipe_Form.php
@@ -11,47 +11,4 @@
 class Recipe_Form extends Form
 {
 
-    public function create()
-    {
-        $fields = '
-            ' . $this->field('image') . '
-            ' . $this->field('title') . '
-            ' . $this->field('short_description') . '
-            <div class="recipe_form_line">
-                ' . $this->field('id_category') . '
-                ' . $this->field('rating') . '
-                ' . $this->field('cook_time') . '
-                ' . $this->field('cooking_method') . '
-                ' . $this->field('servings') . '
-                ' . $this->field('diet') . '
-            </div>
-            <div class="recipe_form_ingredients">' . $this->field('ingredients') . '</div>
-            <div class="recipe_form_preparation">' . $this->field('preparation') . '</div>
-            ' . $this->field('tags');
-        return Form::createForm($fields, ['submit' => __('save'), 'action' => url('cuenta/subir-receta'), 'class'=>'form_site form_site_border']);
-    }
-
-    public function edit()
-    {
-        if ($this->object->id() == '') {
-            return '';
-        }
-        $fields = '
-            ' . $this->field('image') . '
-            ' . $this->field('title') . '
-            ' . $this->field('short_description') . '
-            <div class="recipe_form_line">
-                ' . $this->field('id_category') . '
-                ' . $this->field('rating') . '
-                ' . $this->field('cook_time') . '
-                ' . $this->field('cooking_method') . '
-                ' . $this->field('servings') . '
-                ' . $this->field('diet') . '
-            </div>
-            <div class="recipe_form_ingredients">' . $this->field('ingredients') . '</div>
-            <div class="recipe_form_preparation">' . $this->field('preparation') . '</div>
-            ' . $this->field('tags');
-        return Form::createForm($fields, ['submit' => __('save'), 'action' => url('cuenta/editar-receta/' . $this->object->id()), 'class'=>'form_site form_site_border']);
-    }
-
 }
