9782aa0a85407853dfca79ac11ed272480599586
Raul Leano <theylooksotired@gmail.com>
2024-05-05 21:40:47 +0200

    Fixed versions

--- base/lib/RecipeVersion/RecipeVersion.php
+++ base/lib/RecipeVersion/RecipeVersion.php
@@ -11,4 +11,40 @@
 class RecipeVersion extends Db_Object
 {
 
+    public function persist($persistMultiple = true)
+    {
+        $persist = parent::persist($persistMultiple);
+        if ($persist['status'] == StatusCode::OK) {
+            if ($this->get('preparation_raw') != '') {
+                $preparationLines = explode("\n", $this->get('preparation_raw'));
+                foreach ($preparationLines as $line) {
+                    $line = trim($line);
+                    if ($line != '') {
+                        $preparation = new RecipeVersionPreparation([
+                            'id_recipe_version' => $this->id(),
+                            'step' => $line
+                        ]);
+                        $preparation->persist();
+                    }
+                }
+            }
+            $this->persistSimple('preparation_raw', '');
+            if ($this->get('ingredients_raw') != '') {
+                $ingredientsLines = explode("\n", $this->get('ingredients_raw'));
+                foreach ($ingredientsLines as $line) {
+                    $line = trim($line);
+                    if ($line != '') {
+                        $ingredients = new RecipeVersionIngredient([
+                            'id_recipe_version' => $this->id(),
+                            'ingredient' => $line
+                        ]);
+                        $ingredients->persist();
+                    }
+                }
+            }
+            $this->persistSimple('ingredients_raw', '');
+        }
+        return $persist;
+    }
+
 }
