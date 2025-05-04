5fc43d743074ab2797b17529db21abc933723d11
Raul Leano <theylooksotired@gmail.com>
2025-05-04 22:55:44 +0200

    Fixed minor errors

--- base/lib/Navigation/Navigation_Controller.php
+++ base/lib/Navigation/Navigation_Controller.php
@@ -662,7 +662,7 @@ class Navigation_Controller extends Controller
                 foreach ($items as $item)  {
                     $recipe = (new Recipe)->readFirst(['where' => 'old_id="' . $item->get('id_recipe') . '"']);
                     $result[] = $item->get('id_recipe') . ' - ' . $recipe->id();
-                    $item->persisSimple('id_recipe', $recipe->id());
+                    $item->persistSimple('id_recipe', $recipe->id());
                 }
                 return json_encode($result, JSON_PRETTY_PRINT);
                 break;
