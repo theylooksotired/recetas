b6c37e18d184fdd42f32daad710b17b35f238a0f
Raul Leano <theylooksotired@gmail.com>
2025-12-18 15:15:57 +0100

    Deleted the preparation ad

--- base/lib/Adsense/Adsense.php
+++ base/lib/Adsense/Adsense.php
@@ -13,9 +13,17 @@ class Adsense {
     static public function responsive($type = '') {
         if ($type == 'bottom') return ''; // Disable bottom ads for now
         if ($type == 'preparation') return ''; // Disable preparation ads for now
-        $types = ['top'=>'2176577868', 'ingredients' => '1193850444', 'preparation' => '8717935483', 'bottom' => '5599999310', 'middle' => '8023994431'];
-        $adSlot = (isset($types[$type])) ? $types[$type] : '2090660201';
-        $adSlot = (Parameter::code('country_code') == 'peru') ? $adSlot : '2090660201';
+        $typesPeru = ['top'=>'2176577868', 'ingredients' => '1193850444', 'preparation' => '8717935483', 'bottom' => '5599999310', 'middle' => '8023994431'];
+        $typesHonduras = ['top'=>'4617928436', 'ingredients' => '5820147704', 'middle' => '4673768758'];
+        $adSlotPeru = (isset($typesPeru[$type])) ? $typesPeru[$type] : '2090660201';
+        $adSlotHonduras = (isset($typesHonduras[$type])) ? $typesHonduras[$type] : '2090660201';
+        if (Parameter::code('country_code') == 'honduras') {
+            $adSlot = $adSlotHonduras;
+        } elseif (Parameter::code('country_code') == 'peru') {
+            $adSlot = $adSlotPeru;
+        } else {
+            $adSlot = '2090660201';
+        }
         if (ASTERION_DEBUG) return '<div class="adsense adsenseInline adsenseTest">Ad - ' . (($type!='') ? $type : 'default') . ' - ' . $adSlot . '</div>';
         return '
             <div class="adsense">
